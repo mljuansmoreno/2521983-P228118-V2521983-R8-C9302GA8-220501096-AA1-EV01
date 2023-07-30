a96eee8778fff86cb00424a2f1cc977b08fe52ab
mljuansmoreno <134098557+mljuansmoreno@users.noreply.github.com>
2023-07-29 21:58:33 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-// Import required modules
+// sección de Node.js
+// Importar módulos específicos de Node.js
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// Define the port number for the server
+// Definir el puerto a utilizar
 const port = 3000;
 
-// Create a simple HTTP server
+// Crear servidor HTTP
 const server = http.createServer((req, res) => {
   let filePath = '.' + req.url;
   if (filePath === './') {
     filePath = './index.html'; // Serve index.html by default when accessing the root URL
   }
 
-  // Get the file extension to set the appropriate Content-Type
+  // Adquirir la extensión del archivo solicitado
   const extname = String(path.extname(filePath)).toLowerCase();
   const mimeTypes = {
     '.html': 'text/html',
@@ -23,7 +24,7 @@ const server = http.createServer((req, res) => {
   };
   const contentType = mimeTypes[extname] || 'application/octet-stream';
 
-  // Read and serve the file
+  // Leer y servir el archivo solicitado
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code === 'ENOENT') {
@@ -36,14 +37,14 @@ const server = http.createServer((req, res) => {
         res.end('500 Internal Server Error');
       }
     } else {
-      // Successful response
+      // Respuesta exitosa
       res.writeHead(200, { 'Content-Type': contentType });
       res.end(content);
     }
   });
 });
 
-// Start the server
+// Iniciar el servidor
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
