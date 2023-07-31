2d946b84edc363bc388a3e41ff3b992ee01f2d77
mljuansmoreno <134098557+mljuansmoreno@users.noreply.github.com>
2023-07-31 15:55:46 -0500

    Add files via upload

--- my-angular-project/src/app/app.module.ts
+++ my-angular-project/src/app/app.module.ts
@@ -1,8 +1,30 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { AboutComponent } from './about/about.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     AboutComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; // Make sure this import is present
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +32,8 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]), // Make sure this line is present
   ],
   providers: [],
   bootstrap: [AppComponent]
